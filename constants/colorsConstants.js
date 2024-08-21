91724d7bee03cb11efca065fe02e80288c28db26
Benjamin Canas <32280623+GrandThed@users.noreply.github.com>
2024-08-21 11:55:47 -0300

    migrated to Flutter

--- constants/colorsConstants.js
+++
@@ -1,8 +0,0 @@
-// Standard Colors
-export const BACKGROUND_COLOR = "#313033";
-// Palet
-export const BLUE_COLOR = "#2F5E7E";
-export const LIGHTBLUE_COLOR = "#9CC9D0";
-export const WHITE_COLOR = "#F1ECE1";
-export const YELLOW_COLOR = "#F8BA59";
-export const RED_COLOR = "#CE3B34";
--- firebase/firebaseInit.js
+++
@@ -1,8 +0,0 @@
-import { CONFIGURATION } from "../constants/firebaseConstants";
-
-import firebase from 'firebase/app'
-// import "firebase/firebase";
-import "firebase/auth";
-firebase.initializeApp(CONFIGURATION);
-
-// export const firestore = firebase.firestore()
